63b3a854f135f1f5e83948de255183b034526700
sshkarupa <s.shkarupa@gmail.com>
2015-11-08 13:13:15 +0100

    Added Like button

--- song.rb
+++ song.rb
@@ -9,6 +9,7 @@ class Song
   property :lyrics,       Text
   property :length,       Integer
   property :released_on,  Date
+  property :likes,        Integer, default: 0
 
   def released_on=date
     super Date.strptime(date, '%m/%d/%Y')
@@ -70,4 +71,12 @@ delete '/songs/:id' do
   protected!
   flash[:notice] = "Song deleted" if find_song.destroy
   redirect to('/songs')
+end
+
+post '/songs/:id/like' do
+  @song = find_song
+  @song.likes = @song.likes.next
+  @song.save
+  redirect to"/songs/#{@song.id}" unless request.xhr?
+  slim :like, layout: false
 end
